d384a94baaf55aa25a15975c02ed6c7e75fb2d8e
Danish Amin <danish1658@gmail.com>
2022-11-10 00:34:16 +0530

    v2

--- README.md
+++ README.md
@@ -1,44 +1,59 @@
 
-
+<br>
 <h1 align="center">
-  🚩React Native Dropdown Select List
-</h1>
+  ⭐ React Native Dropdown Select List </h1>
 
 <div align="center">
 
-🟢 React Native Select List Equivalent to Html's Select with options --"
+React Native Select List Equivalent to Html's Select "
 
 </div>
 
 <p align="center" >
-  <kbd>
-    <img
-      src="https://i.imgur.com/JM9fFbH.gif"
-      title="List Demo"
-    >
-  </kbd>
+  <div style="display:flex;justify-content:space-evenly;align-items:center">
+    <div style="display:flex;flex-direction:column;align-items:center;">
+        <img
+          src="https://i.imgur.com/EAT81Nz.gif"
+          title="Multiple Select List Demo"
+          style="width:350px;border-radius:15px"
+        >
+        <em>Multiple Select List Demo.</em>
+    </div>
+    <div style="display:flex;flex-direction:column;align-items:center;">
+        <img
+          src="https://i.imgur.com/qmHreFH.gif"
+          title="Select List Demo"
+          style="width:370px;border-radius:15px"
+        >
+        <em>Select List Demo.</em>
+    </div>
+    
+     
+  </div>
   <br>
-  <em>React Native Dropdown Select List Demo.</em>
+  
 </p>
+<br>
 
 <h4>Light weight and <b>Easy</b> to use dropdown select list.</h4>
 
 -   Style it your way with style props of every view.
 -   Smooth performance on all platforms IOS, Android and Web.
 -   Change Font Family Easily which community picker lacks.
--   Zero dependencies
+-   <b>Zero</b> dependencies
 
-
+<br>
 
 # Compatibility
 
+<br>
 
 |  iOS  | Android | Web | Expo |
 --------|---------|-----|------|
 |  ✅  |    ✅    | ✅ |  ✅  |
 
 
-
+<br>
 
 # 🔌 Installation
 
@@ -53,49 +68,117 @@ OR
 $ yarn add react-native-dropdown-select-list
 ```
 
+<br>
 
-# 😎 Usage
+# 😎 Basic Usage for SelectList
 ```jsx
-import SelectList from 'react-native-dropdown-select-list'
+import { SelectList } from 'react-native-dropdown-select-list'
 
 const App = () => {
 
   const [selected, setSelected] = React.useState("");
   
-  const data = [{key:'1',value:'Jammu & Kashmir'}];
+  const data = [
+      {key:'1', value:'Mobiles', disabled:true},
+      {key:'2', value:'Appliances'},
+      {key:'3', value:'Cameras'},
+      {key:'4', value:'Computers', disabled:true},
+      {key:'5', value:'Vegetables'},
+      {key:'6', value:'Diary Products'},
+      {key:'7', value:'Drinks'},
+  ]
 
   return(
-    <SelectList setSelected={setSelected} data={data} onSelect={() => alert(selected)} />
+    <SelectList 
+        setSelected={(val) => setSelected(val)} 
+        data={data} 
+        save="value"
+    />
+  )
+
+};
+```
+<br>
+
+# 😎 Basic Usage for MultipleSelectList
+```jsx
+import { MultipleSelectList } from 'react-native-dropdown-select-list'
+
+const App = () => {
+
+  const [selected, setSelected] = React.useState("");
+  
+  const data = [
+      {key:'1', value:'Mobiles', disabled:true},
+      {key:'2', value:'Appliances'},
+      {key:'3', value:'Cameras'},
+      {key:'4', value:'Computers', disabled:true},
+      {key:'5', value:'Vegetables'},
+      {key:'6', value:'Diary Products'},
+      {key:'7', value:'Drinks'},
+  ]
+
+  return(
+    <MultipleSelectList 
+        setSelected={(val) => setSelected(val)} 
+        data={data} 
+        save="value"
+        onSelect={() => alert(selected)} 
+        label="Categories"
+    />
   )
 
 };
 ```
 
+<br>
+
+
 For Live `Demo` [(Expo Snack)](https://snack.expo.dev/@danish1658/react-native-dropdown-select-list)
 
-# ⭐ Props
+<br>
+
+# 🔧 General Props
+<i>Applicable on both <b>SelectList</b> & <b>MultipleSelectList</b> Components</i>
+
 | Name | Type | Description |
 | ---- | ---- | ----------- |
+| save| string | Pass ('key' or 'value') to save data of your choice in your local state variable
 | onSelect| Function | Pass any function that you want to trigger immediately after a value is selected
 | placeholder | String | Placeholder text that will be displayed in the select box
 | search | boolean | set to false if you dont want to use search functionality
-| boxStyles| Object| Additional styles for select box
-| inputStyles| Object| Additional styles for text of select box
-| dropdownStyles| Object| Additional styles for dropdown scrollview
-| dropdownItemStyles| Object| Additional styles for dropdown list item
-| dropdownTextStyles| Object| Additional styles for list items text
 | maxHeight| Number | Maximum height of the dropdown wrapper to occupy
 | data| array or array[object]| Data which will be iterated as options of select list
 | setSelected| Function | For Setting the option value which will be stored in your local state
 | searchicon| JSX Element | Pass any JSX to this prop like Text, Image or Icon to show instead of search icon
 | arrowicon| JSX Element | Pass any JSX to this prop like Text, Image or Icon to show instead of chevron icon
+| closeicon| JSX Element | Pass any JSX to this prop like Text, Image or Icon to show instead of close icon
 | searchPlaceholder| String | Custom placeholder text for search TextInput
 | defaultOption| Object | Pass default selected option in key value pair
+| fontFamily| string | Pass font name to apply globally on each text field of component
+| notFoundText| string | Pass your custom message if any search result returns empty
+| dropdownShown| boolean | Control your dropdown ( on & off ) state by changing its value to true or false
 
+<br>
+
+# 🔧 General Style Props
+<i>Applicable on both <b>SelectList</b> & <b>MultipleSelectList</b> Components</i>
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| boxStyles| Object| Additional styles for select box parent wrapper
+| inputStyles| Object| Additional styles for text of selection box
+| dropdownStyles| Object| Additional styles for dropdown scrollview
+| dropdownItemStyles| Object| Additional styles for dropdown single list item
+| dropdownTextStyles| Object| Additional styles for dropdown list items text
+| disabledItemStyles| Object| Additional styles for disabled dropdown list item
+| disabledTextStyles| Object| Additional styles for disabled dropdown list items text
+
+<br>
 
 # 😎 Advanced Usage
 ```jsx
-import SelectList from 'react-native-dropdown-select-list'
+import { SelectList } from 'react-native-dropdown-select-list'
 
 const App = () => {
 
@@ -112,6 +195,7 @@ const App = () => {
     <SelectList 
       onSelect={() => alert(selected)}
       setSelected={setSelected} 
+      fontFamily='lato'
       data={data}  
       arrowicon={<FontAwesome name="chevron-down" size={12} color={'black'} />} 
       searchicon={<FontAwesome name="search" size={12} color={'black'} />} 
@@ -124,10 +208,11 @@ const App = () => {
 };
 ```
 
+<br>
 
 # 😎 Getting Options From Database
 ```jsx
-import SelectList from 'react-native-dropdown-select-list'
+import { SelectList } from 'react-native-dropdown-select-list'
 
 const App = () => {
 
@@ -157,10 +242,37 @@ const App = () => {
 };
 ```
 
+<br>
+
+# 🔧 Additional Props
+<i>Applicable on <b>MultipleSelectList</b> Only</i>
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| label| string| Pass any string to be placed instead of default label
+
+
+<br>
+
+# 🔧 Additional Style Props
+<i>Applicable on <b>MultipleSelectList</b> Only</i>
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| disabledCheckBoxStyles| Object| Additional styles disabled checkbox inside dropdown
+| checkBoxStyles| Object| Additional styles for active checkbox
+| badgeStyles| Object| Additional styles for badges of selected values
+| badgeTextStyles| Object| Additional styles for Text of badges of selected values
+| labelStyles| Object| Additional styles for label of multiple select list
+
+<br>
+
 
 # ▶️ Watch Video
 
 [![Watch the video](https://i.imgur.com/K8Lt2h4.png)](https://www.youtube.com/watch?v=J9raEY-1KPQ&t=499s)
+
+<br>
 
 # 💲 Would you like to support me?
 
@@ -169,3 +281,5 @@ If you would like me come up with similar packages, buy me a cup of coffee to bo
 [![Paypal](https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-100px.png)](https://paypal.me/danishamindar)
 <br><br>
 For Live `Demo` [(Expo Snack)](https://snack.expo.dev/@danish1658/react-native-dropdown-select-list)
+
+<br>
--- components/SelectList.tsx
+++ components/SelectList.tsx
@@ -1,4 +1,4 @@
-import React,{JSXElementConstructor} from 'react';
+import React from 'react';
 import {
     View,
     Text,
@@ -8,87 +8,13 @@ import {
     ScrollView,
     Animated,
     TextInput,
-    ViewStyle} from 'react-native';
+} from 'react-native';
 
-interface SelectListProps  {
-    /**
-    * Fn to set Selected option value which will be stored in your local state
-    */
-    setSelected: React.Dispatch<React.SetStateAction<undefined>>,
+import { SelectListProps } from '..';
 
-    /**
-    * Placeholder text that will be displayed in the select box
-    */
-    placeholder?: string,
+type L1Keys = { key?: any; value?: any; disabled?: boolean | undefined }
 
-    /**
-    * Additional styles for select box
-    */
-    boxStyles?: ViewStyle,
-
-    /**
-    *  	Additional styles for text of select box
-    */
-    inputStyles?: ViewStyle,
-
-    /**
-    *  	Additional styles for dropdown scrollview
-    */
-    dropdownStyles?:ViewStyle,
-
-    /**
-    *  Additional styles for dropdown list item
-    */
-    dropdownItemStyles?: ViewStyle,
-
-    /**
-    * Additional styles for list items text
-    */
-    dropdownTextStyles?: ViewStyle,
-
-    /**
-    * Maximum height of the dropdown wrapper to occupy
-    */
-    maxHeight?: number,
-
-    /**
-    * Data which will be iterated as options of select list
-    */
-    data: Array<{}>,
-
-    /**
-    * The default option of the select list
-    */
-    defaultOption: { key: any, value: any },
-
-    /**
-    * Pass any JSX to this prop like Text, Image or Icon to show instead of search icon
-    */
-    searchicon?: JSX.Element,
-
-    /**
-    *  Pass any JSX to this prop like Text, Image or Icon to show instead of chevron icon
-    */
-    arrowicon?: JSX.Element,
-
-    /**
-    * set to false if you dont want to use search functionality
-    */
-    search?: boolean,
-
-    /**
-    * set to false if you dont want to use search functionality
-    */
-     searchPlaceholder?: string,
-
-    /**
-    * Trigger an action when option is selected
-    */
-    onSelect?: () => void 
-
-}
-
-const SelectList: React.FC<SelectListProps> = ({
+const SelectList: React.FC<SelectListProps> =  ({
         setSelected,
         placeholder,
         boxStyles,
@@ -101,14 +27,21 @@ const SelectList: React.FC<SelectListProps> = ({
         defaultOption,
         searchicon = false,
         arrowicon = false,
+        closeicon = false,
         search = true,
         searchPlaceholder = "search",
+        notFoundText = "No data found",
+        disabledItemStyles,
+        disabledTextStyles,
         onSelect = () => {},
+        save = 'key',
+        dropdownShown = false,
+        fontFamily
     }) => {
 
     const oldOption = React.useRef(null)
     const [_firstRender,_setFirstRender] = React.useState<boolean>(true);
-    const [dropdown, setDropdown] = React.useState<boolean>(false);
+    const [dropdown, setDropdown] = React.useState<boolean>(dropdownShown);
     const [selectedval, setSelectedVal] = React.useState<any>("");
     const [height,setHeight] = React.useState<number>(200)
     const animatedvalue = React.useRef(new Animated.Value(0)).current;
@@ -155,17 +88,32 @@ const SelectList: React.FC<SelectListProps> = ({
   
 
     React.useEffect(() => {
-        if(!_firstRender && defaultOption && oldOption.current != defaultOption.key && oldOption.current != null){
+        if(!_firstRender && defaultOption && oldOption.current != defaultOption.key ){
+            // oldOption.current != null
             oldOption.current = defaultOption.key
             setSelected(defaultOption.key);
             setSelectedVal(defaultOption.value);
         }
         if(defaultOption && _firstRender && defaultOption.key != undefined){
+            
             oldOption.current = defaultOption.key
             setSelected(defaultOption.key);
             setSelectedVal(defaultOption.value);
         }
+        
     },[defaultOption])
+
+    React.useEffect(() => {
+        if(!_firstRender){
+            if(dropdownShown)
+                slidedown();
+            else
+                slideup();
+            
+        }
+        
+    },[dropdownShown])
+
 
 
     return(
@@ -179,7 +127,7 @@ const SelectList: React.FC<SelectListProps> = ({
                                 (!searchicon)
                                 ?
                                 <Image 
-                                    source={require('./assets/images/search.png')}
+                                    source={require('../assets/images/search.png')}
                                     resizeMode='contain'
                                     style={{width:20,height:20,marginRight:7}}
                                 />
@@ -190,21 +138,29 @@ const SelectList: React.FC<SelectListProps> = ({
                             <TextInput 
                                 placeholder={searchPlaceholder}
                                 onChangeText={(val) => {
-                                    let result =  data.filter((item) => {
+                                    let result =  data.filter((item: L1Keys) => {
                                         val.toLowerCase();
                                         let row = item.value.toLowerCase()
                                         return row.search(val.toLowerCase()) > -1;
                                     });
                                     setFilteredData(result)
                                 }}
-                                style={[{padding:0,height:20,flex:1},inputStyles]}
+                                style={[{padding:0,height:20,flex:1,fontFamily},inputStyles]}
                             />
                                 <TouchableOpacity onPress={() => slideup()} >
-                                    <Image 
-                                        source={require('./assets/images/close.png')}
-                                        resizeMode='contain'
-                                        style={{width:15,height:15,paddingLeft:7}}
-                                    />
+
+                                {
+                                    (!closeicon)
+                                    ?
+                                        <Image 
+                                            source={require('../assets/images/close.png')}
+                                            resizeMode='contain'
+                                            style={{width:17,height:17}}
+                                        />
+                                    :
+                                        closeicon
+                                }
+                                   
                                 </TouchableOpacity>
                                 
                            
@@ -213,12 +169,12 @@ const SelectList: React.FC<SelectListProps> = ({
                     </View>
                 :
                     <TouchableOpacity style={[styles.wrapper,boxStyles]} onPress={() => { if(!dropdown){ slidedown() }else{ slideup() } }}>
-                        <Text style={inputStyles}>{ (selectedval == "") ? (placeholder) ? placeholder : 'Select option' : selectedval  }</Text>
+                        <Text style={[{fontFamily},inputStyles]}>{ (selectedval == "") ? (placeholder) ? placeholder : 'Select option' : selectedval  }</Text>
                         {
                             (!arrowicon)
                             ?
                                 <Image 
-                                    source={require('./assets/images/chevron.png')}
+                                    source={require('../assets/images/chevron.png')}
                                     resizeMode='contain'
                                     style={{width:20,height:20}}
                                 />
@@ -232,36 +188,51 @@ const SelectList: React.FC<SelectListProps> = ({
             {
                 (dropdown)
                 ?
-                    <Animated.View style={{maxHeight:animatedvalue}}>
-                        <ScrollView style={[styles.dropdown,dropdownStyles]} contentContainerStyle={{paddingVertical:10,}} nestedScrollEnabled={true}>
+                    <Animated.View style={[{maxHeight:animatedvalue},styles.dropdown,dropdownStyles]}>
+                        <ScrollView  contentContainerStyle={{paddingVertical:10,overflow:'hidden'}} nestedScrollEnabled={true}>
 
                             {
                                 (filtereddata.length >=  1)
                                 ?
-                                filtereddata.map((item,index) => {
+                                filtereddata.map((item: L1Keys,index: number) => {
                                     let key = item.key ?? item.value ?? item;
                                     let value = item.value ?? item;
-                                    return(
-                                        <TouchableOpacity style={[styles.option,dropdownItemStyles]} key={index} onPress={ () => {
-                                            setSelected(key)
-                                            setSelectedVal(value)
-                                            slideup()
-                                            setTimeout(() => setFilteredData(data), 800)
-                                            
-                                        }}>
-                                            <Text style={dropdownTextStyles}>{value}</Text>
-                                        </TouchableOpacity>
-                                    )
+                                    let disabled = item.disabled ?? false;
+                                    if(disabled){
+                                        return(
+                                            <TouchableOpacity style={[styles.disabledoption,disabledItemStyles]} key={index} onPress={ () => {}}>
+                                                <Text style={[{color:'#c4c5c6',fontFamily},disabledTextStyles]}>{value}</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    }else{
+                                        return(
+                                            <TouchableOpacity style={[styles.option,dropdownItemStyles]} key={index} onPress={ () => {
+                                                if(save === 'value'){
+                                                    setSelected(value);
+                                                }else{
+                                                    setSelected(key)
+                                                }
+                                                
+                                                setSelectedVal(value)
+                                                slideup()
+                                                setTimeout(() => {setFilteredData(data)}, 800)
+                                                
+                                            }}>
+                                                <Text style={[{fontFamily},dropdownTextStyles]}>{value}</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    }
+                                    
                                 })
                                 :
                                 <TouchableOpacity style={[styles.option,dropdownItemStyles]} onPress={ () => {
-                                    setSelected("")
+                                    setSelected(undefined)
                                     setSelectedVal("")
                                     slideup()
                                     setTimeout(() => setFilteredData(data), 800)
                                     
                                 }}>
-                                    <Text style={dropdownTextStyles}> No data found</Text>
+                                    <Text style={[{fontFamily},dropdownTextStyles]}>{notFoundText}</Text>
                                 </TouchableOpacity>
                             }
                             
@@ -278,10 +249,14 @@ const SelectList: React.FC<SelectListProps> = ({
     )
 }
 
+
 export default SelectList;
+
 
 const styles = StyleSheet.create({
     wrapper:{ borderWidth:1,borderRadius:10,borderColor:'gray',paddingHorizontal:20,paddingVertical:12,flexDirection:'row',justifyContent:'space-between' },
-    dropdown:{ borderWidth:1,borderRadius:10,borderColor:'gray',marginTop:10},
-    option:{ paddingHorizontal:20,paddingVertical:8 }
+    dropdown:{ borderWidth:1,borderRadius:10,borderColor:'gray',marginTop:10,overflow:'hidden'},
+    option:{ paddingHorizontal:20,paddingVertical:8,overflow:'hidden' },
+    disabledoption:{ paddingHorizontal:20,paddingVertical:8,flexDirection:'row',alignItems:'center', backgroundColor:'whitesmoke',opacity:0.9}
+
 })
